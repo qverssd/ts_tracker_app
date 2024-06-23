7ec31a12053de2d88ab5112cdb42a09ffda1ab6d
qverssd <aleksandr082005@gmail.com>
2024-06-23 13:25:51 +0300

    Note added

---
+++ server/models/Note.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, { timestamps: true });
+
+const Note = model('Note', noteSchema);
+
+export default Note;
