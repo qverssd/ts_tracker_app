a8274f2c5d5cd027def1fe20a51407d88bd27cf8
qverssd <aleksandr082005@gmail.com>
2024-06-24 11:09:24 +0300

    authController almost completed

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,1 +1,27 @@
 import { Request, Response } from "express";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser =async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if(existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+        });
+
+    } catch (error) {
+        
+    }
+}
